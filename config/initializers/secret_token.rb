fa2b8c37380c615d8c59a74b3134164eb367f218
Madeline Simens <MadelineSimens@Madelines-MacBook-Pro-2.local>
2013-10-17 16:00:50 -0400

    First commit of static pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Portfolio::Application.config.secret_key_base = 'ee512b24339c0d8a4f56ce43373d9c28daf7a9d97cb73cb4180dd5a645cf439c0268be9649b8e70c500e0a41fa8fd8f6ca7b655e3ef958fa909ab612d466c0ab'
